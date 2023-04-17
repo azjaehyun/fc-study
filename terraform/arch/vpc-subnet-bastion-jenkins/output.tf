18e31d6e23a70d69de67c792147845b9c7473199
azjaehyun <azjaehyun@gmail.com>
2023-04-18 01:13:47 +0900

    repo modify

--- terraform/arch/vpc-subnet-bastion-jenkins/output.tf
+++ terraform/arch/vpc-subnet-bastion-jenkins/output.tf
@@ -8,3 +8,7 @@ output "ssh_public_key_pem" {
   value = module.aws_key_pair.ssh_public_key_pem
   sensitive = true
 }
+
+output "jenkins_public_ip" {
+  value =module.aws_ec2_public_jenkins_ec2.public_ip
+}
