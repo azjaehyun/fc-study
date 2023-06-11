0557d1f318b64f56d1b2a79e49c4948a43fdeda3
yang jaehyun <jaehyun.yang@bespinglobal.com>
2023-06-11 22:31:17 +0900

    spring docker file add with chapter2

--- chapter-2/project-sample-server/docker/entrypoint.sh
+++ chapter-2/project-sample-server/docker/entrypoint.sh
@@ -6,14 +6,22 @@ fi
 
 mysql_install_db --user mysql > /dev/null
 
+#cat > /tmp/sql <<EOF
+#USE mysql;
+#FLUSH PRIVILEGES;
+#GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
+#UPDATE user SET password=PASSWORD("$MYSQL_ROOT_PASSWORD") WHERE user='root';
+#create database projectdb
+#show grants for root@'%';
+#grant select, insert, update on projectdb.* to 'root'@'%';
+#EOF
+
 cat > /tmp/sql <<EOF
 USE mysql;
 FLUSH PRIVILEGES;
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
-UPDATE user SET password=PASSWORD("$MYSQL_ROOT_PASSWORD") WHERE user='root';
 create database projectdb
-show grants for root@'%';
-grant select, insert, update on projectdb.* to 'root'@'%';
+show grants for developer@'%';
+grant select, insert, update on projectdb.* to 'developer'@'%';
 EOF
 
 mysqld --bootstrap --verbose=0 < /tmp/sql
